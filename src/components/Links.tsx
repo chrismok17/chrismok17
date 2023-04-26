b95bdebcf5eb75b1f2589d1fdacbb0b03319a7bf
Chris M <chrismok97@gmail.com>
2023-04-25 19:08:44 -0700

    added info for about

---
+++ src/components/Links.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Links = () => {
+    return (
+        <>
+            <div className="links">
+                <ul>
+                    <li><a href="https://www.linkedin.com/in/chrismok17/">LinkedIn</a></li>
+                    <li><a href="https://github.com/chrismok17">GitHub</a></li>
+                </ul>
+            </div>
+        </>
+    );
+};
+
+export default Links;
