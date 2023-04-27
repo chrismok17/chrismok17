84fc34d19e11238e54d365986e269e9b8ecd942d
Chris M <chrismok97@gmail.com>
2023-04-27 14:53:59 -0700

    fix build failure 2

--- src/components/FadeInSection.tsx
+++ src/components/FadeInSection.tsx
@@ -4,11 +4,12 @@ const FadeInSection = (props:any) => {
     const [isVisible, setVisible] = React.useState(true);
     const domRef:any = React.useRef();
     React.useEffect(() => {
+      const node = domRef.current;
       const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => setVisible(entry.isIntersecting));
       });
-      observer.observe(domRef.current);
-      return () => observer.unobserve(domRef.current);
+      observer.observe(node);
+      return () => observer.unobserve(node);
     }, []);
     return (
       <div
