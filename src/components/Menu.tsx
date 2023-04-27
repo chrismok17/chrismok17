6626f300b626bc470703f94d56954515d11ebec4
Chris M <chrismok97@gmail.com>
2023-04-26 21:07:15 -0700

    added react-scroll for nav bar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,14 +1,30 @@
 import React from "react";
+import { Link } from "react-scroll";
 
 const Menu = () => {
     return (
         <>
             <div className="menu">
                 <ul>
-                    <li>About Me</li>
-                    <li>Projects</li>
-                    <li>Experience</li>
-                    <li>Contact</li>
+                    <li>
+                        <Link to="about-me" smooth={true} duration={500}>
+                        About Me
+                        </Link></li>
+                    <li>
+                        <Link to="projects" smooth={true} duration={500}>
+                        Projects
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="experience" smooth={true} duration={500}>
+                        Experience
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="links" smooth={true} duration={500}>
+                        Contact
+                        </Link>
+                    </li>
                 </ul>
             </div>
         </>
