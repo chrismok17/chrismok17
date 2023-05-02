9102114328d59547f2506fb3d5355dd02bc62a9d
Chris M <chrismok97@gmail.com>
2023-05-01 17:09:55 -0700

    added scroll effect to menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState, useEffect }  from "react";
 import { Link } from "react-scroll";
 
 const Menu = () => {
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisible] = useState(true);
+
+    useEffect(() => {
+        const handleScroll = () => {
+        const currentScrollPos = window.pageYOffset;
+        const visible = prevScrollPos > currentScrollPos || currentScrollPos < 10;
+        setVisible(visible);
+        setPrevScrollPos(currentScrollPos);
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [prevScrollPos]);
+
+    const className = visible ? "menu" : "menu hidden";
     return (
         <>
-            <div className="menu">
+            <div className={className}>
                 <ul>
                     <li>
                         <Link to="about-me" smooth={true} duration={250} isDynamic={true}>
